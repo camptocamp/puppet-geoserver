326cebcf9f002c1096c7ed2f84f0fc9ead84c62d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-09-17 08:57:07 +0200

    Add unit test for init class

---
+++ spec/classes/geoserver_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'geoserver' do
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/foo',
+        })
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
